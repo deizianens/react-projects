a8f04c1fde6b195961c03bf7aee987b48ed92c52
Deiziane Silva <deiziane_ns@hotmail.com>
2021-03-23 11:59:24 -0300

    adds icons for dice

--- react-yahtzee/src/Die.js
+++ react-yahtzee/src/Die.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import './Die.css';
 
 class Die extends Component {
+  static defaultProps = {
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+  };
+
   handleClick = () => {
     this.props.handleClick(this.props.idx);
   };
@@ -13,7 +22,7 @@ class Die extends Component {
         style={{ backgroundColor: this.props.locked ? 'grey' : 'black' }}
         onClick={this.handleClick}
       >
-        {this.props.val}
+        <i class={`fas fa-dice-${this.props[this.props.val]}`}></i>
       </button>
     );
   }
