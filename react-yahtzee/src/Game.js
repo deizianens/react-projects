d7d2d0b91395f0e98e26e744d55fb58b01e0fdeb
Deiziane Silva <deiziane_ns@hotmail.com>
2021-03-23 10:00:43 -0300

    fix clicking dice

--- react-yahtzee/src/Game.js
+++ react-yahtzee/src/Game.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
-import Dice from "./Dice";
-import ScoreTable from "./ScoreTable";
-import "./Game.css";
+import React, { Component } from 'react';
+import Dice from './Dice';
+import ScoreTable from './ScoreTable';
+import './Game.css';
 
 const NUM_DICE = 5;
 const NUM_ROLLS = 3;
@@ -26,44 +26,42 @@ class Game extends Component {
         smallStraight: undefined,
         largeStraight: undefined,
         yahtzee: undefined,
-        chance: undefined
-      }
+        chance: undefined,
+      },
     };
-    this.roll = this.roll.bind(this);
-    this.doScore = this.doScore.bind(this);
   }
 
-  roll(evt) {
+  roll = (evt) => {
     // roll dice whose indexes are in reroll
-    this.setState(st => ({
+    this.setState((st) => ({
       dice: st.dice.map((d, i) =>
         st.locked[i] ? d : Math.ceil(Math.random() * 6)
       ),
       locked: st.rollsLeft > 1 ? st.locked : Array(NUM_DICE).fill(true),
-      rollsLeft: st.rollsLeft - 1
+      rollsLeft: st.rollsLeft - 1,
     }));
-  }
+  };
 
-  toggleLocked(idx) {
+  toggleLocked = (idx) => {
     // toggle whether idx is in locked or not
-    this.setState(st => ({
+    this.setState((st) => ({
       locked: [
         ...st.locked.slice(0, idx),
         !st.locked[idx],
-        ...st.locked.slice(idx + 1)
-      ]
+        ...st.locked.slice(idx + 1),
+      ],
     }));
-  }
+  };
 
-  doScore(rulename, ruleFn) {
+  doScore = (rulename, ruleFn) => {
     // evaluate this ruleFn with the dice and score this rulename
-    this.setState(st => ({
+    this.setState((st) => ({
       scores: { ...st.scores, [rulename]: ruleFn(this.state.dice) },
       rollsLeft: NUM_ROLLS,
-      locked: Array(NUM_DICE).fill(false)
+      locked: Array(NUM_DICE).fill(false),
     }));
     this.roll();
-  }
+  };
 
   render() {
     return (
@@ -80,7 +78,7 @@ class Game extends Component {
             <div className='Game-button-wrapper'>
               <button
                 className='Game-reroll'
-                disabled={this.state.locked.every(x => x)}
+                disabled={this.state.locked.every((x) => x)}
                 onClick={this.roll}
               >
                 {this.state.rollsLeft} Rerolls Left
