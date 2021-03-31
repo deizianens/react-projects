cbd9004648820094f2ef7985d946d35305b54dec
Deiziane Silva <deiziane_ns@hotmail.com>
2021-03-31 16:24:09 -0300

    adds dropdown to change color format

--- colors-app/src/Palette.js
+++ colors-app/src/Palette.js
@@ -6,22 +6,30 @@ import './Palette.css';
 class Palette extends Component {
   constructor(props) {
     super(props);
-    this.state = { level: 500 };
+    this.state = { level: 500, format: 'hex' };
   }
 
   changeLevel = (newLevel) => {
     this.setState({ level: newLevel });
   };
 
+  changeFormat = (value) => {
+    this.setState({ format: value });
+  };
+
   render() {
-    const { level } = this.state;
+    const { level, format } = this.state;
     const colorBoxes = this.props.palette.colors[level].map((color) => {
-      return <ColorBox background={color.hex} name={color.name} />;
+      return <ColorBox background={color[format]} name={color.name} />;
     });
 
     return (
       <div className='Palette'>
-        <Navbar level={level} changeLevel={this.changeLevel} />
+        <Navbar
+          level={level}
+          changeLevel={this.changeLevel}
+          handleChange={this.changeFormat}
+        />
         <div className='Palette-colors'>{colorBoxes}</div>
       </div>
     );
