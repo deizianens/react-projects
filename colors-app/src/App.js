0f8d5ef09aabe41df76b4e51f494b7392983a855
Deiziane Silva <deiziane_ns@hotmail.com>
2021-03-31 14:32:30 -0300

    adds slider to palette

--- colors-app/src/App.js
+++ colors-app/src/App.js
@@ -1,13 +1,13 @@
-
 import React, { Component } from 'react';
 import Palette from './Palette';
 import seedColors from './seedColors';
+import { generatePalette } from './colorHelper';
 
 class App extends Component {
-  render(){
+  render() {
     return (
       <div>
-        <Palette {...seedColors[0]}/>
+        <Palette palette={generatePalette(seedColors[0])} />
       </div>
     );
   }
