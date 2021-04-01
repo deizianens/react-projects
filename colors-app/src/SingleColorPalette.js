d059ed50b6769c09693c063f25f380ba9780a4e0
Deiziane Silva <deiziane_ns@hotmail.com>
2021-04-01 14:41:39 -0300

    creates footer component and adds it single color

--- colors-app/src/SingleColorPalette.js
+++ colors-app/src/SingleColorPalette.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
+import Navbar from './Navbar';
+import PaletteFooter from './PaletteFooter';
 
 class SingleColorPalette extends Component {
   constructor(props) {
     super(props);
     this._shades = this.gatherShades(this.props.palette, this.props.colorId);
+    this.state = { format: 'hex' };
   }
 
   gatherShades(palette, colorToFilterBy) {
@@ -18,18 +21,27 @@ class SingleColorPalette extends Component {
     return shades.slice(1);
   }
 
+  changeFormat = (value) => {
+    this.setState({ format: value });
+  };
+
   render() {
+    const { format } = this.state;
+    const { paletteName, emoji } = this.props.palette;
+
     const colorBoxes = this._shades.map((color) => (
       <ColorBox
         key={color.id}
         name={color.name}
-        background={color.hex}
+        background={color[format]}
         showLink={false}
       />
     ));
     return (
       <div className='Palette'>
+        <Navbar handleChange={this.changeFormat} showingAllColors={false} />
         <div className='Palette-colors'> {colorBoxes}</div>
+        <PaletteFooter paletteName={paletteName} emoji={emoji} />
       </div>
     );
   }
