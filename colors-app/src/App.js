1f62f1f54528a57d588fae981810b3200abda06b
grrl <deiziane_ns@hotmail.com>
2021-04-04 19:05:38 -0300

    creates new palette form

--- colors-app/src/App.js
+++ colors-app/src/App.js
@@ -5,6 +5,7 @@ import PaletteList from './PaletteList';
 import SingleColorPalette from './SingleColorPalette';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelper';
+import NewPaletteForm from './NewPaletteForm';
 
 class App extends Component {
   findPalette(id) {
@@ -23,6 +24,7 @@ class App extends Component {
             <PaletteList palettes={seedColors} {...routeProps} />
           )}
         />
+        <Route exact path='/palette/new' render={() => <NewPaletteForm />} />
         <Route
           exact
           path='/palette/:id'
