014c0059d544fcbef6326b581b374ec92edf4f3e
grrl <deiziane_ns@hotmail.com>
2021-04-07 19:12:22 -0300

    adds delete to color boxes

--- colors-app/src/DraggableColorBox.js
+++ colors-app/src/DraggableColorBox.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import DeleteIcon from '@material-ui/icons/Delete';
 import { withStyles } from '@material-ui/styles';
 
 const styles = {
@@ -10,15 +11,39 @@ const styles = {
     position: 'relative',
     cursor: 'pointer',
     marginBottom: '-3.5px',
+    '&:hover svg': {
+      color: 'white',
+      transform: 'scale(1.3)',
+    },
+  },
+  boxContent: {
+    position: 'absolute',
+    width: '100%',
+    left: '0px',
+    bottom: '0px',
+    color: 'rgba(0,0,0,0.7)',
+    padding: '10px',
+    textTransform: 'uppercase',
+    letterSpacing: '1px',
+    fontSize: '12px',
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  deleteIcon: {
+    color: 'rgba(0,0,0,0.8)',
+    transition: 'all 0.3s ease-in-out',
   },
 };
 
 class DraggableColorBox extends Component {
   render() {
-    const { color, name, classes } = this.props;
+    const { color, name, classes, handleClick } = this.props;
     return (
       <div className={classes.root} style={{ backgroundColor: color }}>
-        {name}
+        <div className={classes.boxContent}>
+          <span>{name}</span>
+          <DeleteIcon className={classes.deleteIcon} onClick={handleClick} />
+        </div>
       </div>
     );
   }
